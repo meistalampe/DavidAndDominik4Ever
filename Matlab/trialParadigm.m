7123aa080ad0d0aeef4300347e867bd052ac9d55
Dominik <meistalampe.dl@gmail.com>
2018-06-18 11:29:41 +0200

    update

--- Matlab/trialParadigm.m
+++ Matlab/trialParadigm.m
@@ -1,3 +1,24 @@
+% %% file header
+% 
+% % filename:     trialParadigm
+% % author:       dominik limbach
+% % date:         13.06.18
+% 
+% % description:
+% %   - read stimuli files (audioread)
+% %   - process stimuli, same length per vowel (a,i,u)
+% %   - create a null sequence, for waiting periods (1second)
+% %   - write a loop that randomly patches an audio file 
+% %   - audio file: random stimuli, 10x null sequence, repeat 30 times (=
+% %   5min)...60x null sequence, repeat all until every stimulus was
+% %   presented 200 times 
+% %   - implement subroutine that counts stimuli and eliminates a stimulus
+% %   type as soon as 200 is reached ( while loop with break condition which
+% %   becomes true if one variable is at 200,...., second and third loop that
+% %   present remaining variables
+% %   - create new audiofile(audiowrite)
+% %   - create extra row for trigger which hold values according to the vowel
+% %   while it is playing
 %% file header
 
 % filename:     trialParadigm
@@ -23,7 +44,7 @@
 
 %% Clear
 clc;
-clear all;
+clear;
 close all;
 
 %% load stimuli files
@@ -45,21 +66,23 @@ number_of_stim = 3;
 % Fs = 44100;
 Fs = aFs;
 
-% make filler sequence of 10 seconds
-interval = zeros(1,10*Fs);
+% make filler sequence of int_size seconds
+int_size = 5;
+interval = zeros(1,int_size*Fs);
 
 % repetitions of every stimulus
-trial_per_stim = 200;
+trials_per_stim = 200;
 
 % calculate the length of all stimuli for all trials including pauses
-stim_length = trial_per_stim*length(interval); 
+stim_length = trials_per_stim*length(interval); 
 % how many stimuli are presented inbetween breaks
-stim_interval_count = 40;
+stim_per_interval = 30;
 % calculate how many intervals are needed
-number_interval = ceil(trial_per_stim * number_of_stim)/stim_interval_count;
-number_break = number_interval;
+number_intervals = ceil(trials_per_stim * number_of_stim)/stim_per_interval;
+number_break = number_intervals-1;
 % set break to 1 min
-interval_break = zeros(1,6*length(interval));
+break_size = 30 / int_size;
+interval_break = zeros(1,break_size*length(interval));
 % set final length of the clip including breaks
 clip_length = number_of_stim*stim_length + number_break * length(interval_break);
 
@@ -79,7 +102,7 @@ stimuli(6,1:length(i)) = i(:,2);
 
 % initiate audio clip and trigger signal
 % 2 rows for audio signal 1 row for trigger
-% trigger values( a = 1, u = 3, i=5)
+% trigger values( a = 2, u = 4, i=6, pause=1)
 audio_clip = zeros(2,clip_length);
 trigger_signal = zeros(1,clip_length);
 
@@ -89,9 +112,9 @@ loop_condition = true;
 % counter and position variables
 position = 1;
 stim_count = 0;
-a_count = 0;
-u_count = 0;
-i_count = 0;
+count_a = 0;
+count_u = 0;
+count_i = 0;
 
 interval_count = 0;
 
@@ -102,56 +125,227 @@ stim = randi(number_of_stim);
 
 switch stim
     case 1
-        if(a_count <= trial_per_stim)
+        if(count_a <= trials_per_stim)
             audio_clip(1,position:(position+length(interval)-1)) = stimuli(1,:);
             audio_clip(2,position:(position+length(interval)-1)) = stimuli(2,:);
-            trigger_signal(1,position:(position+length(a)-1)) = 1;
+            %trigger_signal(1,position:(position+length(a)-1)) = 1;
+            trigger_signal(1,position:(position + Fs*0.02)) =1;
 
             position = position + length(interval);
-            a_count = a_count + 1;
+            count_a = count_a + 1;
             stim_count = stim_count + 1;
         end
         
     case 2
-        if(u_count <= trial_per_stim)
+        if(count_u <= trials_per_stim)
             audio_clip(1,position:(position+length(interval)-1)) = stimuli(3,:);
             audio_clip(2,position:(position+length(interval)-1)) = stimuli(4,:);
-            trigger_signal(1,position:(position+length(u)-1)) = 3;
+            %trigger_signal(1,position:(position+length(u)-1)) = 3;
+            trigger_signal(1,position:(position + Fs*0.04)) =1;
 
             position = position + length(interval);
-            u_count = u_count + 1;
+            count_u = count_u + 1;
             stim_count = stim_count + 1;
         end
     case 3
-        if(i_count <= trial_per_stim)
+        if(count_i <= trials_per_stim)
             audio_clip(1,position:(position+length(interval)-1)) = stimuli(5,:);
             audio_clip(2,position:(position+length(interval)-1)) = stimuli(6,:);
-            trigger_signal(1,position:(position+length(i)-1)) = 5;
+            %trigger_signal(1,position:(position+length(i)-1)) = 5;
+            trigger_signal(1,position:(position + Fs*0.06)) =1;
 
             position = position + length(interval);
-            i_count = i_count + 1;
+            count_i = count_i + 1;
             stim_count = stim_count + 1;
         end
 end
 
 
-if(stim_count == stim_interval_count)
-    audio_clip(1,position:(position+length(pause)-1)) = 0;
-    audio_clip(2,position:(position+length(pause)-1)) = 0;
-    trigger_signal(1,position:(position+length(pause)-1)) = 0;
+if(stim_count == stim_per_interval)
+    audio_clip(1,position:(position+length(interval_break)-1)) = 0;
+    audio_clip(2,position:(position+length(interval_break)-1)) = 0;
+    trigger_signal(1,position:(position+length(interval_break)-1)) = 0;
+    trigger_signal(1,position:(position + Fs*0.1)) = 1;
     
-    position = position + length(pause);
+    position = position + length(interval_break);
     stim_count = 0;
     interval_count = interval_count + 1;
 end
 
-if(interval_count == number_interval)
+if(interval_count == number_intervals)
     loop_condition = false;
+    
 end
 
 end
+% 
+% % save 
+% save('vowelStimulus.mat','-v7.3');
+% 
+% % write audio file
+% filename = 'clip.wav';
+% audiowrite(filename,audio_clip(1,:),aFs);
+% 
+% 
+% % trigger signal 
+% 
+% filenamet = 'trigger.wav';
+% audiowrite(filenamet,trigger_signal(1,:),aFs);
 
-% write audio file
-filename = 'clip.wav';
-audiowrite(filename,audio_clip(1,:),aFs);
+time = 1:(length(trigger_signal));
+time = time./aFs;
 
+figure;
+hold on;
+plot(time(150000000:end),trigger_signal(150000000:end));
+plot(time(150000000:end),audio_clip(1,150000000:end));
+ylabel('amplitude');
+xlabel('time [s]');
+legend('trigger signal','audio signal')
+% 
+% (158000000:end)
+
+% %% Clear
+% clc;
+% clear;
+% close all;
+% 
+% %% load stimuli files
+% 
+% [a,aFs] = audioread('a.wav');
+% [u,uFs] = audioread('u.wav');
+% [i,iFs] = audioread('i.wav');
+% 
+% % number of different stimuli
+% number_of_stim = 3;
+% 
+% % play command to test files
+% % player=audioplayer(a,aFs);
+% % play(player,[1 (get(player,'SampleRate')*3)]);
+% 
+% %% create filler sequence
+% 
+% % set sampling frequency if not uniform
+% % Fs = 44100;
+% Fs = aFs;
+% 
+% % make filler sequence of 10 seconds
+% interval = zeros(1,5*Fs);
+% 
+% % repetitions of every stimulus
+% trial_per_stim = 300;
+% 
+% % calculate the length of all stimuli for all trials including pauses
+% stim_length = trial_per_stim*length(interval); 
+% % how many stimuli are presented inbetween breaks
+% stim_interval_count = 30;
+% % calculate how many intervals are needed
+% number_interval = ceil(trial_per_stim * number_of_stim)/stim_interval_count;
+% number_break = number_interval;
+% % set break to 1 min
+% interval_break = zeros(1,6*length(interval));
+% % set final length of the clip including breaks
+% clip_length = number_of_stim*stim_length + number_break * length(interval_break);
+% 
+% %% audio clip composition
+% 
+% % compose stimuli intervals
+% stimuli = zeros(number_of_stim*2, length(interval)); % stereo signal so 2 rows per stim
+% % a
+% stimuli(1,1:length(a)) = a(:,1);
+% stimuli(2,1:length(a)) = a(:,2);
+% % u
+% stimuli(3,1:length(u)) = u(:,1);
+% stimuli(4,1:length(u)) = u(:,2);
+% % i
+% stimuli(5,1:length(i)) = i(:,1);
+% stimuli(6,1:length(i)) = i(:,2);
+% 
+% % initiate audio clip and trigger signal
+% % 2 rows for audio signal 1 row for trigger
+% % trigger values( a = 1, u = 3, i=5)
+% audio_clip = zeros(2,clip_length);
+% trigger_signal = zeros(1,clip_length);
+% 
+% 
+% loop_condition = true;
+% 
+% % counter and position variables
+% position = 1;
+% stim_count = 0;
+% count_a = 0;
+% count_u = 0;
+% count_i = 0;
+% 
+% interval_count = 0;
+% 
+% while loop_condition == true
+%  
+% % random stimulus    
+% stim = randi(number_of_stim);
+% 
+% switch stim
+%     case 1
+%         if(count_a <= trial_per_stim)
+%             audio_clip(1,position:(position+length(interval)-1)) = stimuli(1,:);
+%             audio_clip(2,position:(position+length(interval)-1)) = stimuli(2,:);
+%             %trigger_signal(1,position:(position+length(a)-1)) = 1;
+%             trigger_signal(1,position) = 1;
+% 
+%             position = position + length(interval);
+%             count_a = count_a + 1;
+%             stim_count = stim_count + 1;
+%         end
+%         
+%     case 2
+%         if(count_u <= trial_per_stim)
+%             audio_clip(1,position:(position+length(interval)-1)) = stimuli(3,:);
+%             audio_clip(2,position:(position+length(interval)-1)) = stimuli(4,:);
+%             %trigger_signal(1,position:(position+length(u)-1)) = 3;
+%             trigger_signal(1,position) = 3;
+% 
+%             position = position + length(interval);
+%             count_u = count_u + 1;
+%             stim_count = stim_count + 1;
+%         end
+%     case 3
+%         if(count_i <= trial_per_stim)
+%             audio_clip(1,position:(position+length(interval)-1)) = stimuli(5,:);
+%             audio_clip(2,position:(position+length(interval)-1)) = stimuli(6,:);
+%             %trigger_signal(1,position:(position+length(i)-1)) = 5;
+%             trigger_signal(1,position) = 5;
+% 
+%             position = position + length(interval);
+%             count_i = count_i + 1;
+%             stim_count = stim_count + 1;
+%         end
+% end
+% 
+% 
+% if(stim_count == stim_interval_count)
+%     audio_clip(1,position:(position+length(pause)-1)) = 0;
+%     audio_clip(2,position:(position+length(pause)-1)) = 0;
+%     trigger_signal(1,position:(position+length(pause)-1)) = 0;
+%     
+%     position = position + length(pause);
+%     stim_count = 0;
+%     interval_count = interval_count + 1;
+% end
+% 
+% if(interval_count == number_interval)
+%     loop_condition = false;
+% end
+% 
+% end
+% 
+% % write audio file
+% filename = 'clip.wav';
+% audiowrite(filename,audio_clip(1,:),aFs);
+% 
+% % trigger signal 
+% time = 1:length(trigger_signal);
+% time = time./aFs;
+% 
+% figure;
+% plot(time,trigger_signal);
+% label
