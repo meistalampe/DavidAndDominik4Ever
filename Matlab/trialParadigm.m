e3f981d851bec54c3e343c5f25ab3700c8958fe5
Dominik <meistalampe.dl@gmail.com>
2018-06-19 10:09:06 +0200

    update

--- Matlab/trialParadigm.m
+++ Matlab/trialParadigm.m
@@ -19,6 +19,11 @@
 % %   - create new audiofile(audiowrite)
 % %   - create extra row for trigger which hold values according to the vowel
 % %   while it is playing
+% %   - create a time delay of 10% of the intervall time that is randomly
+% %   weaven in and counters low frequency overlapping with the signal
+% %   - normalize audiosignal to a amplitude of 1 and adjust loudness(dB)
+% %   according to a sample with randomized phase (fft, randi phase,ifft -
+% %   detect loudness)
 %% file header
 
 % filename:     trialParadigm
@@ -103,7 +108,8 @@ stimuli(6,1:length(i)) = i(:,2);
 % initiate audio clip and trigger signal
 % 2 rows for audio signal 1 row for trigger
 % trigger values( a = 2, u = 4, i=6, pause=1)
-audio_clip = zeros(2,clip_length);
+audio_clip_L = zeros(1,clip_length);
+audio_clip_R = zeros(1,clip_length);
 trigger_signal = zeros(1,clip_length);
 
 
@@ -118,52 +124,76 @@ count_i = 0;
 
 interval_count = 0;
 
+% create delay parameter
+% set boundaries for delay
+minimum = (0.05*length(interval));
+maximum = (0.15*length(interval));
+% create a vector with random delay times for each stimulus
+N = (trials_per_stim*number_of_stim)+1;
+delay_time = randi([minimum maximum],1,N);
+% add all delays
+delay_sum = sum(delay_time);
+% create empty vector to compensate for delay
+delay = zeros(1,delay_sum);
+
+delay_count = 1;
+
+% preallocation
+audio_clip_L = [audio_clip_L delay];
+audio_clip_R = [audio_clip_R delay];
+trigger_signal = [trigger_signal delay];
+
 while loop_condition == true
  
 % random stimulus    
 stim = randi(number_of_stim);
+position = position + delay_time(delay_count);
+
 
 switch stim
     case 1
         if(count_a <= trials_per_stim)
-            audio_clip(1,position:(position+length(interval)-1)) = stimuli(1,:);
-            audio_clip(2,position:(position+length(interval)-1)) = stimuli(2,:);
+            audio_clip_L(1,position:(position+length(interval)-1)) = stimuli(1,:);
+            audio_clip_R(1,position:(position+length(interval)-1)) = stimuli(2,:);
             %trigger_signal(1,position:(position+length(a)-1)) = 1;
             trigger_signal(1,position:(position + Fs*0.02)) =1;
 
             position = position + length(interval);
             count_a = count_a + 1;
             stim_count = stim_count + 1;
+            delay_count = delay_count + 1;
         end
         
     case 2
         if(count_u <= trials_per_stim)
-            audio_clip(1,position:(position+length(interval)-1)) = stimuli(3,:);
-            audio_clip(2,position:(position+length(interval)-1)) = stimuli(4,:);
+            audio_clip_L(1,position:(position+length(interval)-1)) = stimuli(3,:);
+            audio_clip_R(1,position:(position+length(interval)-1)) = stimuli(4,:);
             %trigger_signal(1,position:(position+length(u)-1)) = 3;
             trigger_signal(1,position:(position + Fs*0.04)) =1;
 
             position = position + length(interval);
             count_u = count_u + 1;
             stim_count = stim_count + 1;
+            delay_count = delay_count + 1;
         end
     case 3
         if(count_i <= trials_per_stim)
-            audio_clip(1,position:(position+length(interval)-1)) = stimuli(5,:);
-            audio_clip(2,position:(position+length(interval)-1)) = stimuli(6,:);
+            audio_clip_L(1,position:(position+length(interval)-1)) = stimuli(5,:);
+            audio_clip_R(1,position:(position+length(interval)-1)) = stimuli(6,:);
             %trigger_signal(1,position:(position+length(i)-1)) = 5;
             trigger_signal(1,position:(position + Fs*0.06)) =1;
 
             position = position + length(interval);
             count_i = count_i + 1;
             stim_count = stim_count + 1;
+            delay_count = delay_count + 1;
         end
 end
 
 
 if(stim_count == stim_per_interval)
-    audio_clip(1,position:(position+length(interval_break)-1)) = 0;
-    audio_clip(2,position:(position+length(interval_break)-1)) = 0;
+    audio_clip_L(1,position:(position+length(interval_break)-1)) = 0;
+    audio_clip_R(1,position:(position+length(interval_break)-1)) = 0;
     trigger_signal(1,position:(position+length(interval_break)-1)) = 0;
     trigger_signal(1,position:(position + Fs*0.1)) = 1;
     
@@ -178,7 +208,32 @@ if(interval_count == number_intervals)
 end
 
 end
+
+% normalize amplitude
+audio_clip_L = audio_clip_L ./ max(abs(audio_clip_L));
+audio_clip_R = audio_clip_R ./ max(abs(audio_clip_R));
 % 
+% % fft
+% fft_L = fft(audio_clip_L);
+% amp = 2*abs(fft_L)/length(fft_L);
+% phs = angle(fft_L);
+% 
+% for j = length(phs)
+% phs(j) = (pi+pi).*rand(1,1) + -pi;
+% end
+% ifft_L = zeros(1,length(fft_L));
+%  
+% for i = length(fft_L)
+%    real = amp(i);
+%    imag = phs(i);
+%    ifft_L(i) = real + imag;
+%     
+% end
+% 
+% ifft_L = ifft(ifft_L);
+% 
+% audiowrite('test.wav',ifft_L(1,:),aFs);
+% % 
 % % save 
 % save('vowelStimulus.mat','-v7.3');
 % 
@@ -197,11 +252,14 @@ time = time./aFs;
 
 figure;
 hold on;
-plot(time(150000000:end),trigger_signal(150000000:end));
-plot(time(150000000:end),audio_clip(1,150000000:end));
+plot(time(158000000:end),trigger_signal(158000000:end));
+plot(time(158000000:end),audio_clip_L(158000000:end));
 ylabel('amplitude');
 xlabel('time [s]');
 legend('trigger signal','audio signal')
+
+
+
 % 
 % (158000000:end)
 
